9e4c282851fcb71667adb0a590a7de9c2ce5e833
vladislav20221 <117448294+vladislav20221@users.noreply.github.com>
2023-04-20 19:07:54 +0600

    Update MergeSort_1.java

--- MergeSort/MergeSort_1.java
+++ MergeSort/MergeSort_1.java
@@ -6,7 +6,7 @@ public class MergeSort {
     public Comparable[] sort ( Comparable[] array ) {
         if ( array == null ) return null;
         if ( array.length < 2 ) return array;        
-        int mid = array.length >> 1;        // Середина массива.
+        int mid = array.length>>1;        // Середина массива.
         // Левая половина.
         Comparable[] arrayA = Arrays.copyOfRange( array, 0, mid );
         // Правая половина.
@@ -21,20 +21,20 @@ public class MergeSort {
     private Comparable[] merge ( final Comparable[] arrayA, final Comparable[] arrayB ) {
         int positionA = 0;
         int positionB = 0;
-        int positionB = 0;
+        int positionM = 0;
         Comparable[] buffer = new Comparable [ arrayA.length+arrayB.length ];
         while ( positionA < arrayA.length && positionB < arrayB.length ) {
             if ( arrayA[positionA].compareTo(arrayB[positionB]) < 0 ) {                
-                buffer[positionB++] = arrayA[positionA++];
+                buffer[positionM++] = arrayA[positionA++];
             } else {
-                buffer[positionB++] = arrayB[positionB++];
+                buffer[positionM++] = arrayB[positionB++];
             }
         }
         // Дозапись частей массива.
         while ( positionA < arrayA.length )
-            buffer[positionB++] = arrayA[positionA++];
+            buffer[positionM++] = arrayA[positionA++];
         while ( positionB < arrayB.length )
-            buffer[positionB++] = arrayB[positionB++];
+            buffer[positionM++] = arrayB[positionB++];
         return buffer;
     }
 }
