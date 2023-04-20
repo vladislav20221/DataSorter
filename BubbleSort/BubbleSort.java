97fcfe48d56ea55b70bd50c30cdbc7d1c69a979d
vladislav20221 <117448294+vladislav20221@users.noreply.github.com>
2023-04-20 20:02:25 +0600

    Update BubbleSort.java

--- BubbleSort/BubbleSort.java
+++ BubbleSort/BubbleSort.java
@@ -7,9 +7,11 @@ public class BubbleSort {
     public static void sort ( final Comparable[] array ) {
         for ( int i = 0; i < array.length-1; i++ ) {
             for ( int j = 1; j < array.length-i; j++ ) {
-                Comparable temp = array[j-1];
-                array[j-1] = array[j];
-                array[j] = temp;
+                if ( array[j-1].compareTo( array[j] ) > 0 ) {
+                    Comparable temp = array[j-1];
+                    array[j-1] = array[j];
+                    array[j] = temp;
+                }                
             }
         }
     }
