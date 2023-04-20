837c8c1bb4017130062768cca1938ac80b890e0e
vladislav20221 <117448294+vladislav20221@users.noreply.github.com>
2023-04-20 19:52:46 +0600

    12

---
+++ BubbleSort/BubbleSort.java
@@ -0,0 +1,16 @@
+/**
+ * Алгоритм пузырьковой сортировки.
+ * @author User
+ */
+public class BubbleSort {
+
+    public static void sort ( final Comparable[] array ) {
+        for ( int i = 0; i < array.length-1; i++ ) {
+            for ( int j = 1; j < array.length-i; j++ ) {
+                Comparable temp = array[j-1];
+                array[j-1] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+}
